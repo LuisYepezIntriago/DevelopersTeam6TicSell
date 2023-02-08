ab5992fe2a4f555aa211abcfd2243778dc52752b
SilviaYunga95 <slyunga@espe.edu.ec>
2023-02-08 12:33:01 -0500

    update
    provider

--- 06-Code/02WebServices/TicSell/src/routes/provideRoute.js
+++ 06-Code/02WebServices/TicSell/src/routes/provideRoute.js
@@ -8,3 +8,4 @@ router.get("/:id", providerController.getProviderById);
 router.put("/:id", providerController.updateProviderById);
 router.delete("/:id", providerController.deleteProviderById);
 export default router;
+
